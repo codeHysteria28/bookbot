bd51a79fbb173b83fac7acb6e6a42be236897671
Branislav Buna <46035047+codeHysteria28@users.noreply.github.com>
2025-12-05 23:23:31 +0000

    refactor: extract word counting logic into a separate function

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ def get_book_text(file_path):
     
     return file_contents
 
+def count_words(text):
+    text_split = text.split()
+    word_count = len(text_split)
+
+    return word_count
+
 def main():
     file = get_book_text("./books/frankenstein.txt")
+    words = count_words(file)
 
-    print(file)
+    print(f"Found {words} total words")
 
 main()
